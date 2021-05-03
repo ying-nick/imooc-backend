51d6de47e1c3ab7829a0e2b8decbf0d68ef51d1d
DM <276061317@qq.com>
2021-05-03 14:01:16 +0800

    1

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const Koa = require('koa')
 const app = new Koa()
 const Router = require('koa-router')
-const cors=require('koa2-cors')
+const cors = require('koa2-cors')
 const router = new Router()
-app.use(cors({
-  origin: ['http://localhost:9528'],
-  credentials:true
-}))
+//小程序环境id
+const Env = 'cloud1-8g4btmo4df7e108b'
+app.use(
+	cors({
+		origin: ['http://localhost:9528'],
+		credentials: true,
+	})
+)
+//全局中间件
+app.use(async (ctx, next) => {
+	// console.log('quanju')
+	ctx.state.env = Env
+	await next()
+})
 const playlist = require('./controller/playlist')
 //通过方法导入路由
-router.use('/playlist',playlist.routes())
+router.use('/playlist', playlist.routes())
 
 app.use(router.routes())
 app.use(router.allowedMethods())
-
-
 
 app.listen(3000, () => {
 	console.log('running...')
